338eaf452f463f6b2d4c97f1d0a485efc25f7402
nanoblock <yongseok.jang@grep.services>
2016-10-26 03:47:45 +0900

    completed page link view drop right

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,15 +2,14 @@ class Image < ActiveRecord::Base
   has_many :ocrs, dependent: :destroy
   has_many :clickables, dependent: :destroy
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  # validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "120x180!" }, default_url: "/images/:style/missing.png"
 
   Paperclip.interpolates :id_sha1  do |attachment, style|
     Digest::SHA1.hexdigest(attachment.instance.id.to_s)
   end
 
   has_attached_file :photo, 
-  styles: { original: "400x650!", medium: "400x650!", thumb: "120x180!" }, #, thumb: "400x650!"
+  styles: { original: ["400x650!", :jpeg], medium: ["400x650!", :jpeg], thumb: ["120x180!", :jpeg] }, #, thumb: "400x650!"
   path: ":rails_root/public/images/:id_sha1/:style.:extension", #upload server image path
   url: "/images/:id_sha1/:style.:extension"
   
